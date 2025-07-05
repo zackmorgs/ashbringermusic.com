b3f36977ce5f9888be49878d428ec7dabf12488a
zackmorgs <zackmorgenthaler@gmail.com>
2025-07-05 14:08:50 -0500

    initial commit

---
+++ assets/script.js
@@ -0,0 +1,31 @@
+const handleNavState = () => {
+    let isOpen = false;
+    const navToggle = document.getElementById('nav_toggle');
+    navToggle.addEventListener('click', () => {
+        isOpen = !isOpen;
+        navToggle.innerHTML = isOpen == true ? 'Close' : 'Menu';
+        const nav = document.getElementById('nav_main');
+        nav.classList.toggle('open');
+    });
+};
+
+const handleActiveNavLink = () => {
+    const currentPath = window.location.pathname;
+    const navLinks = document.querySelectorAll('#nav_main .nav-link');
+    navLinks.forEach(link => {
+        const linkPath = link.getAttribute('href');
+        if (linkPath === currentPath) {
+            link.classList.add('active');
+        } else {
+            link.classList.remove('active');
+        }
+    });
+};
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    document.body.classList.toggle('loaded');
+    handleNavState();
+    handleActiveNavLink();
+});
