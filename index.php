5c5acb4ddec2a741c1355552ae1d9030a704b148
zackmorgs <zackmorgenthaler@gmail.com>
2025-07-06 13:08:08 -0500

    Add homepage and single post templates
    - Created a new homepage template (page-home.php) featuring band information, social links, press mentions, featured releases, and merchandise.
    - Implemented a single post template (single.php) to display individual blog posts with title, date, author, content, categories, and comments section.
    - Added necessary HTML structure and PHP functions to ensure dynamic content rendering.

--- index.php
+++ index.php
@@ -1,108 +1,34 @@
-<?php get_header(); ?>
-<header id="hero_main" class="hero">
-    <div class="hero-content">
-        <img class="logo" src="<?php echo get_template_directory_uri(); ?>/assets/svg/ashbringer_main_logo.svg" />
-        <br />
-        <h3>Minneapolis based </h3>
-        <h2>False Black Metal</h2>
-        <br />
-        <ul class="btn-group">
-            <li>
-                <a href="/listen" class="btn btn-primary">
-                    <img src="<?php echo get_template_directory_uri(); ?>/assets/svg/icon-play.svg" alt="Play"
-                        class="icon" />
-                </a>
-            </li>
-        </ul>
-    </div>
-</header>
-<section id="main_social">
-    <div class="content text-center">
-        <!-- <h3>Social</h3> -->
-        <ul class="btn-group">
-            <li>
-                <a href="https://www.facebook.com/ashbringermusic" class="btn btn-social" target="_blank">
-                    <img src="<?php echo get_template_directory_uri(); ?>/assets/svg/icon-fb.svg" alt="Facebook"
-                        class="icon" />
-                </a>
-            </li>
+<?php
+/* Template Name: Blog Page */
+get_header(); ?>
 
-            <li>
-                <a href="https://www.instagram.com/ashbringermusic/" class="btn btn-social" target="_blank">
-                    <img src="<?php echo get_template_directory_uri(); ?>/assets/svg/icon-instagram.svg" alt="Instagram"
-                        class="icon" />
-                </a>
-            </li>
-            <li>
-                <a href="https://ashbringermusic.bandcamp.com" target="_blank" class="btn btn-social">
-                    <img src="<?php echo get_template_directory_uri(); ?>/assets/img/bandcamp_b&w.png" alt="Bandcamp"
-                        class="icon" />
-                </a>
-            </li>
-        </ul>
-        <a href="https://www.youtube.com/@ashbringermusic" class="btn btn-social" target="_blank">
-            <img src="<?php echo get_template_directory_uri(); ?>/assets/svg/icon-youtube.svg" alt="YouTube"
-                class="icon" />
-        </a>
-    </div>
-</section>
-<section id="content_press">
-    <div class="content">
-        <h4 class="text-center">Press</h4>
-        <ul id="press_list">
-            <li>
-                <a href="https://metalinjection.net/?s=ashbringer">
-                    <img src="<?php echo get_template_directory_uri(); ?>/assets/img/metal_injection-logo.png"
-                        alt="As seen on Metal Injection" class="icon" />
-                </a>
-            </li>
-            <li>
-                <a href="https://www.metalsucks.net/?s=ashbringer">
-                    <img src="<?php echo get_template_directory_uri(); ?>/assets/svg/logo-metal_sucks.svg"
-                        alt="As seen on MetalSucks" class="icon" />
-                </a>
-            </li>
-            <li>
-                <a href="https://www.metalunderground.com/bands/details.cfm?bandid=25229&tab=news">
-                    <img src="<?php echo get_template_directory_uri(); ?>/assets/img/metalundergroundcom_logo.gif"
-                        alt="As seen on Metal Underground" class="icon" />
-                </a>
-            </li>
-            <li>
-                <a href="https://www.invisibleoranges.com/?s=ashbringer">
-                    <img src="<?php echo get_template_directory_uri(); ?>/assets/img/invisible-oranges-logo.png"
-                        alt="As seen on Metal Underground" class="icon" />
-                </a>
-            </li>
-            <li>
-                <a href="https://www.angrymetalguy.com/?s=ashbringer">
-                    <img src="<?php echo get_template_directory_uri(); ?>/assets/img/angry-metal-guy-logo.webp"
-                        alt="As seen on Metal Underground" class="icon" />
-                </a>
-            </li>
-        </ul>
-    </div>
-</section>
-<section id="featured_release">
-    <div class="content">
-        <h4 class="text-center">Featured Release</h4>
-        <a href="https://ashbringermusic.bandcamp.com/album/vacant-2025-remaster" class="release">
-            <img src="<?php echo get_template_directory_uri(); ?>/assets/img/vacant_2025_remaster.jpg" />
-        </a>
-        <h5 class="text-center">Vacant (2025 Remaster)</h5>
-        <p class="text-center">Released on April 8, 2025</p>
-    </div>
-</section>
-<section id="featured_merch">
-    <div class="content">
-        <h4 class="text-center">Featured Merch</h4>
-        <img class="merch" src="<?php echo get_template_directory_uri(); ?>/assets/img/merch_rain.jpg"
-            alt="Featured Merch">
-        <br />
-        <a href="https://ashbringermusic.bandcamp.com/merch/rain-4-sided-long-sleeve" class="btn">
-            Buy Now
-        </a>
-    </div>
-</section>
-<!-- https://www.youtube.com/@ashbringermusic -->
+<main class="container">
+    <section class="blog-posts">
+        <header class="hero">
+            <h3 id="latest_posts">Latest Posts</h3>
+        </header>
+        <div class="post-list">
+            <?php
+            // Query the last 3 posts
+            $latest_posts = new WP_Query(query: [
+                'posts_per_page' => 6
+            ]);
+
+            if ($latest_posts->have_posts()):
+                while ($latest_posts->have_posts()):
+                    $latest_posts->the_post(); ?>
+                    <a href="<?php the_permalink(); ?>">
+                        <article class="post content">
+                            <h2><?php the_title(); ?></h2>
+                            <div class="meta"><?php the_time(format: 'F j, Y'); ?> by <?php the_author(); ?></div>
+                            <div class="excerpt"><?php the_excerpt(); ?></div>
+                        </article>
+                    </a>
+                <?php endwhile; ?> wp_reset_postdata(); else: ?>
+                <p>No posts found.</p>
+            <?php endif; ?>
+        </div>
+    </section>
+</main>
+
 <?php get_footer(); ?>
