0fc086a5745a6d013e8089d6439866c218fdc9ea
zackmorgs <zackmorgenthaler@gmail.com>
2025-07-11 06:11:39 -0500

    updated iframe

--- page-listen.php
+++ page-listen.php
@@ -15,9 +15,9 @@
             <br />
             <br />
 
-            <iframe style="border-radius:12px"
+            <iframe style="border-radius:12px; width: 100%;"
                 src="https://open.spotify.com/embed/artist/697R3cOlkWzOAkXztLzeg2?utm_source=generator&theme=0"
-                width="100%" height="352" frameBorder="0" allowfullscreen=""
+                 height="352" frameBorder="0" allowfullscreen=""
                 allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
                 loading="lazy"></iframe>
         </div>
